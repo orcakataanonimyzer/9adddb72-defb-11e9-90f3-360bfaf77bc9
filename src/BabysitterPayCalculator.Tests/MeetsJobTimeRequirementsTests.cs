2999ce21d0a13f1223c6e805212729b87a82922a
someone <something@apa.com>
2019-06-22 18:13:44 -0500

    - Add MeetsJobTimeRequirements test for invalid start time job.

--- src/BabysitterPayCalculator.Tests/MeetsJobTimeRequirementsTests.cs
+++ src/BabysitterPayCalculator.Tests/MeetsJobTimeRequirementsTests.cs
@@ -38,5 +38,26 @@ namespace BabysitterPayCalculator.Tests
             var expectedValue = true;
             Assert.AreEqual(expectedValue, metRequirements);
         }
+
+
+        [TestMethod]
+        public void Babysitter_MeetsJobTimeRequirements_ReturnsFalseForInvalidStartTimeJob()
+        {
+            // Arrange.
+            var job = new Job
+            {
+                StartDateTime = new DateTime(2019, 6, 20, 16, 0, 0), // 6/20/2019 - 4:00:00PM
+                EndDateTime = new DateTime(2019, 6, 21, 3, 0, 0) // 6/21/2019 - 3:00:00AM
+            };
+            Babysitter.MinimumStartTime = new TimeSpan(17, 0, 0); // 5:00:00PM
+            Babysitter.MaximumEndTime = new TimeSpan(3, 0, 0); // 3:00:00AM
+
+            // Act.
+            var metRequirements = Babysitter.MeetsJobTimeRequirements(job);
+
+            // Assert.
+            var expectedValue = false;
+            Assert.AreEqual(expectedValue, metRequirements);
+        }
     }
 }
