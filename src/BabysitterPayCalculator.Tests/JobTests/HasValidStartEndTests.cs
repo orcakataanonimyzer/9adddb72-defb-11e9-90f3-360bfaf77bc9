31c15e237ceaedceed32713d4fb6d58d6ae4082e
someone <something@apa.com>
2019-06-22 21:23:21 -0500

    - Refactor JobTests into separate concerns.

---
+++ src/BabysitterPayCalculator.Tests/JobTests/HasValidStartEndTests.cs
@@ -0,0 +1,50 @@
+﻿using BabysitterPayCalculator.Library;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BabysitterPayCalculator.Tests.JobTests
+{
+    [TestClass]
+    public class HasValidStartEndTests
+    {
+        private Job Job;
+
+        /// <summary>
+        ///     Initializes before each test.
+        /// </summary>
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Job = new Job()
+            {
+                StartDateTime = new DateTime(2019, 6, 20, 1, 0, 0),
+                EndDateTime = new DateTime(2019, 6, 20, 2, 0, 0)
+            };
+        }
+
+        [TestMethod]
+        public void Job_HasValidStartAndEnd_ReturnsTrueForValidStartAndEnd()
+        {
+            // Act.
+            var hasValidStartAndEnd = Job.HasValidStartAndEnd();
+
+            // Assert.
+            var expectedValue = true;
+            Assert.AreEqual(expectedValue, hasValidStartAndEnd);
+        }
+
+        [TestMethod]
+        public void Job_HasValidStartAndEnd_ReturnsFalseForInvalidStartAndEnd()
+        {
+            // Arrange.
+            Job.EndDateTime = new DateTime(2019, 6, 20, 1, 0, 0);
+
+            // Act.
+            var hasValidStartAndEnd = Job.HasValidStartAndEnd();
+
+            // Assert.
+            var expectedValue = false;
+            Assert.AreEqual(expectedValue, hasValidStartAndEnd);
+        }
+    }
+}
--- src/BabysitterPayCalculator.Tests/JobTests/JobPropertyTests.cs
+++ src/BabysitterPayCalculator.Tests/JobTests/JobPropertyTests.cs
@@ -5,7 +5,7 @@ using System;
 namespace BabysitterPayCalculator.Tests
 {
     [TestClass]
-    public class JobTests
+    public class JobPropertyTests
     {
         private Job Job;
 
@@ -35,31 +35,6 @@ namespace BabysitterPayCalculator.Tests
             // Assert.
             var expectedEndDateTime = new DateTime(2019, 6, 20, 2, 0, 0);
             Assert.AreEqual(expectedEndDateTime, Job.EndDateTime);
-        }
-        
-        [TestMethod]
-        public void Job_HasValidStartAndEnd_ReturnsTrueForValidStartAndEnd()
-        {
-            // Act.
-            var hasValidStartAndEnd = Job.HasValidStartAndEnd();
-
-            // Assert.
-            var expectedValue = true;
-            Assert.AreEqual(expectedValue, hasValidStartAndEnd);
-        }
-
-        [TestMethod]
-        public void Job_HasValidStartAndEnd_ReturnsFalseForInvalidStartAndEnd()
-        {
-            // Arrange.
-            Job.EndDateTime = new DateTime(2019, 6, 20, 1, 0, 0);
-
-            // Act.
-            var hasValidStartAndEnd = Job.HasValidStartAndEnd();
-
-            // Assert.
-            var expectedValue = false;
-            Assert.AreEqual(expectedValue, hasValidStartAndEnd);
         }
     }
 }
